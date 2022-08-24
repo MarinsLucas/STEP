b88c29245a8d610a040fef5f850d1e81cd6bc2fd
Lucas Marins <lucasmarinsramalho@gmail.com>
2022-08-23 22:09:00 -0300

    reading values from file

--- Form1.cs
+++ Form1.cs
@@ -12,17 +12,62 @@ namespace STEP
             InitializeComponent();
         }
 
-        private void drawGraphic(int[] information)
+        //Draws a simple linear graphic representing a single dimension of telemetry data
+        private void drawLinearGraphic(int[] information, Pen pen)
         {
             Bitmap bitmap = new Bitmap(1000, 800, System.Drawing.Imaging.PixelFormat.Format32bppPArgb);
             Graphics graphics = Graphics.FromImage(bitmap);
-            Pen pen = new Pen(Color.Blue, 2);
-            for (int i = 0; i < information.Length; i++)
+            for (int i = 1; i < information.Length; i++)
             {
                 graphics.DrawLine(pen, i * 10, information[i - 1], (i + 1) * 10, information[i]);
             }
 
             bitmap.Save("graphic.png");
+        }
+
+        private void readingFile(String path)
+        {
+            /*
+            using (FileStream fs = File.OpenRead(path))
+            {
+                byte[] b = new byte[1024];
+                UTF8Encoding temp = new UTF8Encoding(true);
+                String text = null;
+                while (fs.Read(b, 0, b.Length) > 0)
+                {
+                    text += temp.GetString(b) + " ";
+                }
+                textBox1.Text = text;
+            }
+            */
+            int [] information;
+            using (TextReader reader = File.OpenText(path))
+            {
+                string text = reader.ReadToEnd();
+                string[] bits = text.Split(';'); //get all the numbers from file in strings
+                information = new int[bits.Length];
+                for (int i = 0; i < bits.Length; i++)
+                {
+                    try
+                    {
+                        information[i] = int.Parse(bits[i]);
+                    }
+                    catch (System.FormatException e)
+                    {
+                        continue;
+                    }
+                }
+            }
+
+
+            //Debug with random numbers
+            /*
+             * for(int i=0;i<150;i++)
+            {
+                information[i] = new Random().Next(720);
+            }
+            */
+            drawLinearGraphic(information, new Pen(Color.White, 1));
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -48,19 +93,8 @@ namespace STEP
             //opening openFileDialog and displaying the message on the screen
             if (openFileDialog1.ShowDialog() == DialogResult.OK)
             {
-                string path = openFileDialog1.FileName;
-                //Open the stream and read it.  
-                using (FileStream fs = File.OpenRead(path))
-                {
-                    byte[] b = new byte[1024];
-                    UTF8Encoding temp = new UTF8Encoding(true);
-                    String text = null;
-                    while (fs.Read(b, 0, b.Length) > 0)
-                    {
-                        text += temp.GetString(b) + " ";
-                    }
-                    textBox1.Text = text; 
-                }
+                //Open the stream and read 
+                readingFile(openFileDialog1.FileName);
             }
 
         }
--- REPORT.md
+++ REPORT.md
@@ -3,5 +3,9 @@
 1) Open file from windows explorer
 2) Load the information from the file
 3) Create a graphic from the file
+	- For creating the graphic, we need to set de range of data and study how to display it in a readable way
+	- I need to create the axys
+	- There will be two kinds of graphics (for while): the Linear and the G-G diagram.
+		- For the g-g diagram, I need to set the null value in the center of the screen, and all the points around this center
 4) Print the graphic in the screen
 	5) Create the G-G graphic or the linear graphic of a axys of the movement 
